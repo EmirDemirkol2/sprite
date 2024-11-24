971125d51ebe7d8ccabcf179a9aa21ce80dc040a
EmirDemirkol2 <emirdemirkol799@gmail.com>
2024-11-24 19:27:59 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+})
+let x = 0
+let y = 0
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(100)
+            led.plot(x, y)
+            basic.pause(100)
+        }
+    }
+})
